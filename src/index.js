72812910e39f43284aba5d73e62b8062cbd8882e
Nastya Filippova <nastya.filippova87@gmail.com>
2022-07-31 23:09:38 +0300

    final change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  let objectBrackets = Object.fromEntries(bracketsConfig);
+  let stack = [];
+ 
+  str.forEach(element => {
+    if (element === objectBrackets[stack[stack.length - 1]]) {
+      stack.pop()
+    }  else {
+      stack.push(element)
+    };
+  });
+  return stack.length === 0;
 }
